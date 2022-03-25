02525c3225d820eb7039b736c7b2d52ed03b97bc
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2022-03-26 00:51:13 +0700

    add allow cors

--- api/convert.js
+++ api/convert.js
@@ -3,7 +3,24 @@ const { getPdfFromHtml } = require('../service/convert')
 // Cache header max age
 const maxAge = 24 * 60 * 60
 
-module.exports = async (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	)
+	if (req.method === 'OPTIONS') {
+		res.status(200).end()
+		return
+	}
+	return await fn(req, res)
+}
+
+const handler = async (req, res) => {
 	try {
 		// Only allow GET requests
 		if (req.method === 'GET') return res.status(405).end()
@@ -40,3 +57,5 @@ module.exports = async (req, res) => {
 		res.status(500).send('Error: Please try again.')
 	}
 }
+
+module.exports = allowCors(handler)
